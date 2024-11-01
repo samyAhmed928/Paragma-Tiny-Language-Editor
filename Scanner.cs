823065366cbca55ccf1a36f8495b6f6e55156643
samyAhmed928 <71889972+samyAhmed928@users.noreply.github.com>
2024-11-01 23:59:22 +0200

    add token class

--- Scanner.cs
+++ Scanner.cs
@@ -8,14 +8,7 @@ using System.Windows.Forms;
 
 namespace Tiny_language_project
 {
-    public enum Token_Class
-    {
-        Int, Float, String, Read, Write, Repeat, Until, If, Elseif, Else, End, Then, Return, Endl, Main,
-        Dot, Semicolon, Comma, LSquareBrackets, RSquareBrackets, LCurlyBrackets, RCurlyBrackets, LParanthesis, RParanthesis, EqualOp, LessThanOp,
-        GreaterThanOp, NotEqualOp, PlusOp, MinusOp, MultiplyOp, DivideOp, Assignment_op, NotEqual, And_op, OR_op
-        , Idenifier, Number, Comment
 
-    }
     public class Token
     {
         public string lexim;
---
+++ Token_Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiny_language_project
+{
+	public enum Token_Class
+	{
+		Int, Float, String, Read, Write, Repeat, Until, If, Elseif, Else, End, Then, Return, Endl, Main,
+		Dot, Semicolon, Comma, LSquareBrackets, RSquareBrackets, LCurlyBrackets, RCurlyBrackets, LParanthesis, RParanthesis, EqualOp, LessThanOp,
+		GreaterThanOp, NotEqualOp, PlusOp, MinusOp, MultiplyOp, DivideOp, Assignment_op, NotEqual, And_op, OR_op
+		, Idenifier, Number, Comment
+
+	}
+}
